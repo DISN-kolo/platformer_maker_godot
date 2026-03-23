4f770a1d18ca126ed77500889b623779973c7dfc
Artem Kozin <akozin@car10s1.42barcelona.com>
2026-03-23 17:26:49 +0100

    lvl5 with pickupable added

--- PickUp/pick_up_able.gd
+++ PickUp/pick_up_able.gd
@@ -3,4 +3,6 @@ extends Node2D;
 @export var pickup_id : Enums.PickupableID;
 
 func _ready() -> void:
+	if (pickup_id in PlayerMetrics.has_items):
+		queue_free();
 	$PUSprite.texture = load(Enums.pickupSprites[pickup_id]);
