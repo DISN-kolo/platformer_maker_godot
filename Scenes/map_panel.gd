1d553dd82e159561e3023438c3d206a2687d75a6
DISN-kolo <82868527+DISN-kolo@users.noreply.github.com>
2026-03-27 13:56:32 +0100

    moved the map generation into 'map storage', made it generate it all at once (primitive tree logic), made a separate display function, clean-up

--- Scenes/map_panel.gd
+++ Scenes/map_panel.gd
@@ -3,59 +3,17 @@ extends Panel
 @onready var level_block_ps: PackedScene = preload("res://Scenes/MiniMapStuff/level_block_panel.tscn");
 @onready var level_entry_ps: PackedScene = preload("res://Scenes/MiniMapStuff/level_entry_panel.tscn");
 
-@onready var main: MainNode = $"../../../../.."
-var ll: BaseLevel;
-# wow. terrible          ^^^^^^^^^^^^^^^^^^ FIXME
-
 func _ready() -> void:
-	Signals.level_loaded.connect(generate_panel_for_minimap);
+	MinimapStorage.generate();
+	draw_minimap();
 
-func get_docker_pos_s(dockers: Array[Docker]) -> Array[Vector2]:
-	var ret: Array[Vector2] = [];
-	for docker in dockers:
-		ret.append(docker.position);
-	return ret;
-
-func get_dockers_info(dockers: Array[Docker]) -> Array[Array]:
-	var ret: Array[Array] = [];
-	for docker in dockers:
-		ret.append([docker.connect_to_level_id, docker.connect_to_docker_id]);
-	return ret;
-
-func generate_panel_for_minimap() -> void:
-	ll = main.loaded_level;
-	var id = ll.level_id;
-	var eid = PlayerMetrics.last_spawn_id;
-	if (id in PlayerMetrics.generated_panels_for_level_ids):
-		return ;
-	else:
-		PlayerMetrics.generated_panels_for_level_ids.append(id);
-	var temp_level_panel: Panel = level_block_ps.instantiate();
-	var camlims: Vector4i = ll.get_cam_limits();
-	var tl: Vector2 = Vector2(camlims.x, camlims.y);
-	if (!(id in MinimapStorage.tl_s.keys())):
-		MinimapStorage.tl_s[id] = tl;
-	var dockers: Array[Docker] = ll.get_dockers();
-	temp_level_panel.size = Vector2(camlims.z - camlims.x, camlims.w - camlims.y)/Settings.minimap_scale;
-	if (!(id in MinimapStorage.dockers_info.keys())):
-		MinimapStorage.dockers_info[id] = get_dockers_info(dockers);
-	if (!(id in MinimapStorage.docker_pos_s.keys())):
-		MinimapStorage.docker_pos_s[id] = get_docker_pos_s(dockers);
-	if (id != 0):
-		var prev_id: int = MinimapStorage.dockers_info[id][eid][0];
-		var old_level_tl: Vector2 = MinimapStorage.tl_s[prev_id]/Settings.minimap_scale;
-		var old_level_pos: Vector2 = MinimapStorage.level_pos_s[prev_id];
-		var old_docker_pos: Vector2 = MinimapStorage.docker_pos_s[prev_id][dockers[eid].connect_to_docker_id]/Settings.minimap_scale;
-		var this_docker_pos: Vector2 = MinimapStorage.docker_pos_s[id][eid]/Settings.minimap_scale;
-		temp_level_panel.position = (
-			(old_docker_pos - old_level_tl + old_level_pos)
-			- (this_docker_pos - tl/Settings.minimap_scale)
-		);
-	if (!(id in MinimapStorage.level_pos_s.keys())):
-		MinimapStorage.level_pos_s[id] = temp_level_panel.position;
-	add_child(temp_level_panel);
-	for docker in dockers:
-		var temp_entry_panel: Panel = level_entry_ps.instantiate();
-		temp_entry_panel.position = (docker.position - tl)/Settings.minimap_scale;
-		temp_entry_panel.position -= temp_entry_panel.size/2;
-		temp_level_panel.add_child(temp_entry_panel);
+func draw_minimap() -> void:
+	for id: int in MinimapStorage.level_pos_s.keys():
+		var level_panel: Panel = level_block_ps.instantiate();
+		level_panel.position = MinimapStorage.level_pos_s[id];
+		level_panel.size = MinimapStorage.level_sizes[id];
+		add_child(level_panel);
+		for j: int in range(MinimapStorage.mm_docker_pos_s[id].size()):
+			var entry_panel: Panel = level_entry_ps.instantiate();
+			entry_panel.position = MinimapStorage.mm_docker_pos_s[id][j] - entry_panel.size / 2;
+			level_panel.add_child(entry_panel);
--- Singletons/minimap_storage.gd
+++ Singletons/minimap_storage.gd
@@ -1,11 +1,75 @@
 extends Node;
 
-##Minimap's level blocks' positions. Key = id of level. Maybe, should all be relative to level0?
+##Minimap's level blocks' positions (in mm space).
+##Key = id of level. All relative to level0.
 var level_pos_s: Dictionary[int, Vector2] = {};
-##Minimap's docker blocks' positions, RELATIVE. Key = id of level, and then id of docker.
-var docker_pos_s: Dictionary[int, Array] = {};
-##Docker info: [0] = connects to which level id?, [1] = corresponds to what docker id?
+
+##Minimap's level blocks' sizes (in mm space).
+##Key = id of level.
+var level_sizes: Dictionary[int, Vector2] = {};
+
+##Docker positions for the minimap (in level space).
 ##Key = id of level, and then id of docker.
+var docker_pos_s: Dictionary[int, Array] = {};
+
+##Docker info: [0] = connects to which level id?, [1] = corresponds to what docker id?
+##Key = id of level, and then id of docker (and then the ultimate key, 0 or 1)
 var dockers_info: Dictionary[int, Array] = {};
-##Top-left corners of levels.
+
+##Top-left corners of levels, in level space, for building the map
 var tl_s: Dictionary[int, Vector2] = {};
+
+##Docker positions for the minimap (in minimap space, relative to level block panel).
+##Key = id of level, and then id of docker.
+var mm_docker_pos_s: Dictionary[int, Array] = {};
+
+func _get_docker_pos_s(dockers: Array[Docker]) -> Array[Vector2]:
+	var ret: Array[Vector2] = [];
+	for docker: Docker in dockers:
+		ret.append(docker.position);
+	return ret;
+
+func _get_dockers_info(dockers: Array[Docker]) -> Array[Array]:
+	var ret: Array[Array] = [];
+	for docker: Docker in dockers:
+		ret.append([docker.connect_to_level_id, docker.connect_to_docker_id]);
+	return ret;
+
+func generate() -> void:
+	var visited: Array[int] = [];
+	# queue entries: [level_id, entry_docker_id] (-1 for level 0, no entry docker)
+	var queue: Array = [[0, -1]];
+	while (queue.size() > 0):
+		var entry: Array = queue.pop_front();
+		var id: int = entry[0];
+		var eid: int = entry[1];
+		if (id in visited):
+			continue ;
+		visited.append(id);
+		var temp_ll: BaseLevel = load("res://Scenes/Levels/level_%d.tscn" % [id]).instantiate();
+		var camlims: Vector4i = temp_ll.get_cam_limits();
+		var tl: Vector2 = Vector2(camlims.x, camlims.y);
+		var dockers: Array[Docker] = temp_ll.get_dockers();
+		tl_s[id] = tl;
+		dockers_info[id] = _get_dockers_info(dockers);
+		docker_pos_s[id] = _get_docker_pos_s(dockers);
+		level_sizes[id] = Vector2(camlims.z - camlims.x, camlims.w - camlims.y) / Settings.minimap_scale;
+		var pos: Vector2 = Vector2(0, 0);
+		if (id != 0):
+			var prev_id: int = dockers_info[id][eid][0];
+			var old_level_tl: Vector2 = tl_s[prev_id] / Settings.minimap_scale;
+			var old_level_pos: Vector2 = level_pos_s[prev_id];
+			var old_docker_pos: Vector2 = docker_pos_s[prev_id][dockers[eid].connect_to_docker_id] / Settings.minimap_scale;
+			var this_docker_pos: Vector2 = docker_pos_s[id][eid] / Settings.minimap_scale;
+			pos = (
+				(old_docker_pos - old_level_tl + old_level_pos)
+				- (this_docker_pos - tl / Settings.minimap_scale)
+			);
+		level_pos_s[id] = pos;
+		var mm_dockers: Array[Vector2] = [];
+		for docker: Docker in dockers:
+			mm_dockers.append((docker.position - tl) / Settings.minimap_scale);
+			if (!(docker.connect_to_level_id in visited)):
+				queue.append([docker.connect_to_level_id, docker.connect_to_docker_id]);
+		mm_docker_pos_s[id] = mm_dockers;
+		temp_ll.free();
